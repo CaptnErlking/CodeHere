302041809db56a2e38a4a3e375289325636d6e4e
Captn_Erlking <abaryaman59@gmail.com>
2025-08-31 15:55:00 +0530

    Arranged repo

--- C++/GFG/array/temp.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int arr[2] = { 1, 2 };
-    cout << 0 [arr] << ", " << 1 [arr] << endl;
-    return 0;
-}
--- C++/RandomPractice/speedtest.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout << "Hi guys this is just to test the speed of my VS code";
-}
